13dfd01fc487e5bfe7d68573f4f6f9e953fbea0a
Saradhi <saradhi0001069@gmail.com>
2022-06-18 09:10:04 +0530

    rm node modules

--- Backend/Backend/storage/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const Connection = mongoose.connect(
-  "mongodb+srv://Devesh26:2T-K-uVH56*tmf9@cluster0.tbzsaga.mongodb.net/?retryWrites=true&w=majority"
-);
-
-module.exports = Connection;
